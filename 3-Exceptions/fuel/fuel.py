8d52eed3357b6fbcef89577eed6432d658c9709b
evans-au <nathan.evans75@education.nsw.gov.au>
2026-02-17 01:54:11 +0000

    fuel completed

--- 3-Exceptions/fuel/fuel.py
+++ 3-Exceptions/fuel/fuel.py
@@ -1,13 +1,44 @@
 def main():
-    try: 
-        fraction = input('Enter a fraction: ')
+    fraction = get_fraction()
+    x, y = split_fraction(fraction)
+    z = fraction_to_percentage(x,y)
+    print(f'Fuel Level: {display(z)}')
+
+    
+def get_fraction():
+    fraction = input('Enter a fraction: ')
+    return fraction
+
+def split_fraction(fraction):
+    try:
         x,y = fraction.split('/')
-        x = int(x)
+        x, y = int(x), int(y)
     except ValueError:
-        print('x is not an integer')
+        print('ValueError: A value is not an integer')
+        main()
+    else:
+        return x, y
     
+def fraction_to_percentage(x,y):
+    try:
+        z = x / y * 100
+    except ZeroDivisionError:
+        print('ZeroDivisionError: Cannot divide by zero')
+        main()
+    else:
+        if x > y:
+            print('ValueError: Numerator is larger than denominator')
+            main()
+        z = int(z)
+        return(z)
+
+def display(z):
+    if z <= 1:
+        return('E')
+    elif z >= 99:
+        return('F')
+    else:
+        return(f'{z}%')
     
-
-
 if __name__ == "__main__":
     main()
