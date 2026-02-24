62d6ccac7d6ee10dc3f305fffa511a3fc7661513
evans-au <nathan.evans75@education.nsw.gov.au>
2026-02-24 01:31:08 +0000

    groceries input and loop

--- 3-Exceptions/grocery/grocery.py
+++ 3-Exceptions/grocery/grocery.py
@@ -1,9 +1,18 @@
-
+items = {}
 def main():
     while True:
         try:
             item = input('Item: ')
         except EOFError:
             continue
+        item = item.upper()
+        if item not in items:
+            items.update({item:1})
+        else:
+            quant = items[item]
+            quant = quant + 1
+            items.update({item:quant})
+        print(items)
+            
 if __name__ == '__main__':
     main()
