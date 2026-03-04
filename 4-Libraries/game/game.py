04e5a0afab5e6bd61e2c5206c9afe49ae4d15f75
evans-au <nathan.evans75@education.nsw.gov.au>
2026-03-04 00:10:58 +0000

    game.py completed

--- 4-Libraries/game/game.py
+++ 4-Libraries/game/game.py
@@ -1,7 +1,45 @@
 from random import randint
 
 def main():
-    return
+    while True:
+        try:
+            level = int(input('Level: '))
+            if level < 1:
+                raise ValueError
+        except ValueError:
+            print('ValueError: You must enter a positive integer')
+            continue
+        break
+
+    target = randint(0,level)
+
+    while True:
+        try:
+            guess = int(input('Guess? '))
+            if guess < 1:
+                raise ValueError
+        except ValueError:
+            print(f'ValueError: Guess must be a positive integer between 1 and {level}')
+        
+        if guess == target:
+            print('Just right!')
+            break
+        elif guess > level:
+            print(f'Too large, your guess should be between 1 and {level}.')
+            continue
+        elif guess < target:
+            print('Too Small!')
+            continue
+        elif guess > target:
+            print('Too large!')
+            continue
+        else:
+            print("Don't know, guess again.")
+            continue
+    print('Thank you for playing!')
+
+
+        
 
 if __name__ == '__main__':
     main()
