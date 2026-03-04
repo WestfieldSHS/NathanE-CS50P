c486bae219f448253e4f41be3034382d7b92ead1
evans-au <nathan.evans75@education.nsw.gov.au>
2026-03-04 04:04:53 +0000

    professor completed

--- 4-Libraries/professor/professor.py
+++ 4-Libraries/professor/professor.py
@@ -2,11 +2,15 @@ from random import randint
 
 
 def main():
+    score = 0
     level = get_level()
-    num1 = generate_integer(level)
-    num2 = generate_integer(level)
-    print(num1)
-    print(num2)
+    for i in range(10):
+        num1 = generate_integer(level)
+        num2 = generate_integer(level)
+        answer = num1 + num2
+        point = question(num1,num2,answer)
+        score = score + point
+    print(f'Score: {score}/10')
 
 
 
@@ -39,7 +43,30 @@ def generate_integer(level):
     num_int = int(num_str)
     return(num_int)
 
+def question(num1, num2, answer):
+    #num1,num2,answer = int(num1),int(num2),int(answer)
+    attempt = 1
+    while attempt < 4:
+        try:
+            guess = int(input(f'{num1} + {num2} = '))
+        except ValueError:
+            attempt = attempt + 1
+            print('EEE')
+            continue
+        if guess == answer:
+            print('Correct!')
+            point = 1
+            return point
+        else:
+            attempt = attempt + 1
+            print('EEE')
+            continue
+    print(f'{num1} + {num2} = {answer}')
+    point = 0
+    return point
 
+
+        
 
 
 if __name__ == "__main__":
